0dd13b5087cfe56db7a490c66b93da678605dbd8
lili-hyelinKim <156570163+lili-hyelinKim@users.noreply.github.com>
2024-03-09 03:08:35 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.77 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,36 +1,14 @@
 class Solution {
-     public String solution(String s) {
-            String answer = "";
+  public String solution(String s) {
 
-            String[] list=s.split(" ",-1);
+        String answer = "";
+        int cnt = 0;
+        String[] array = s.split("");
 
-
-            String[] list2=new String[list.length];
-
-
-
-
-            for(int i=0;i<list.length;i++) {
-                list2[i]="";
-                for(int j=0;j<list[i].length();j++) {   
-                    if(j%2==0) {
-                        list2[i]+=Character.toUpperCase(list[i].charAt(j));
-                    }
-                    else {
-                        list2[i]+=Character.toLowerCase(list[i].charAt(j));
-                    }
-                }
-            }
-
-            for(int i=0;i<list2.length;i++) {
-                            if(i!=list2.length-1) {
-                    answer+=list2[i]+" ";
-                }
-                else {
-                    answer+=list2[i];
-                }
-            }
-
-            return answer;
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
         }
+      return answer;
+  }
 }
