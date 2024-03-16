cbbfbc63245bf5766b8c842cb84bdc26f1b8f646
lili-hyelinKim <156570163+lili-hyelinKim@users.noreply.github.com>
2024-03-16 20:44:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.31 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,22 +1,9 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        for(int i=0; i<s.length(); i++){
-            if(s.contains("zero")) s=s.replace("zero", "0");
-            else if(s.contains("one")) s=s.replace("one","1");
-            else if(s.contains("two")) s=s.replace("two","2");
-            else if(s.contains("three")) s=s.replace("three","3");
-            else if(s.contains("four")) s=s.replace("four","4");
-            else if(s.contains("five")) s=s.replace("five","5");
-            else if(s.contains("six")) s=s.replace("six","6");
-            else if(s.contains("seven")) s=s.replace("seven","7");
-            else if(s.contains("eight")) s=s.replace("eight","8");
-            else if(s.contains("nine")) s=s.replace("nine","9");
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-       
-        answer = Integer.parseInt(s);
-        
-        
-        return answer;
+        return Integer.parseInt(s);
     }
 }
